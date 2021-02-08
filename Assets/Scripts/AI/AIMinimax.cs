a3ebf2c7e954b18e1c8ccb0d9f88df74f7807921
KiwiCowan <kylecowan3@gmail.com>
2021-02-08 07:31:08 +0200

    V9
    Added Evaluator and some minmax

---
+++ Assets/Scripts/AI/AIMinimax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMinimax : AIPlayer
+{
+    public int MaxDepth { get; set; } = 5;
+
+    Evaluator evaluator = new Evaluator();
+    public override Vector2Int GetMove(GameState gameState, char player)
+    {       
+        return Minimax(gameState, MaxDepth,player, int.MaxValue, int.MaxValue);
+    }
+
+    Vector2Int Minimax(GameState gameState, int depth, char player,int alpha, int beta)
+    {
+        
+        if (gameState.IsGameOver() || depth == 0)
+        {
+            //return gameState.lastMove;
+        }
+        return new Vector2Int(0, 0);
+    }
+}
--- Assets/Scripts/Evaluator.cs
+++ Assets/Scripts/Evaluator.cs
@@ -10,14 +10,276 @@ public class Evaluator
     {
         score = 0;
 
-        int[] scores = new int[4];
+        int[] scores = new int[2];
 
-        //scores[0] +=
+        scores[0] += EvaluateEndGame(gameState);
+        scores[1] += EvaluateMobility(gameState);
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (Mathf.Abs(scores[i]) == WIN_SCORE)
+            {
+                score = scores[i];
+                return true;
+            }
+            score += scores[i];
+        }
+
         return false;
     }
-    
-    public int EvaluateCol()
+
+    int EvaluatePosition(GameState gameState)
+    {
+        float xScore = 0;
+        float oScore = 0;
+                
+        int xNumMoves = gameState.GetPossibleMoves('x').Count;
+        int oNumMoves = gameState.GetPossibleMoves('o').Count;
+
+        char[,] board = gameState.Board;
+
+
+        // if any moves remain for either player, eval as normal
+        if (xNumMoves > 0 || oNumMoves > 0)
+        {
+            // check for posession of corners
+            if (board[0, 0] == 'x' || board[0, 0] == 'X')
+            {
+                xScore += 10;
+            }
+            else if (board[0, 0] == 'o' || board[0, 0] == 'O')
+            {
+                oScore += 10;
+            }
+
+            if (board[0, board.GetLength(1)] == 'x' || board[0, board.GetLength(1)] == 'X')
+            {
+                xScore += 10;
+            }
+            else if (board[0, board.GetLength(1)] == 'o' || board[0, board.GetLength(1)] == 'O')
+            {
+                oScore += 10;
+            }
+
+            if (board[board.GetLength(0), 0] == 'x' || board[board.GetLength(0), 0] == 'X')
+            {
+                xScore += 10;
+            }
+            else if (board[board.GetLength(0), 0] == 'o' || board[board.GetLength(0), 0] == 'O')
+            {
+                oScore += 10;
+            }
+
+            if (board[board.GetLength(0), board.GetLength(1)] == 'x' || board[board.GetLength(0), board.GetLength(1)] == 'X')
+            {
+                xScore += 10;
+            }
+            else if (board[board.GetLength(0), board.GetLength(1)] == 'o' || board[board.GetLength(0), board.GetLength(1)] == 'O')
+            {
+                oScore += 10;
+            }
+
+            // check number of available moves
+            xScore += xNumMoves;
+            oScore += oNumMoves;
+
+            // check for number of tiles possesed
+            for (int h = 0; h < board.GetLength(1); h++)
+            {
+                for (int w = 0; w < board.GetLength(0); w++)
+                {
+                    if (gameState.Board[w, h] == 'x')
+                    {
+                        xScore += 0.01f;
+                    }
+
+                    if (gameState.Board[w, h] == 'X')
+                    {
+                        xScore += 0.02f;
+                    }
+
+                    if (gameState.Board[w, h] == 'o')
+                    {
+                        oScore += 0.01f;
+                    }
+
+                    if (gameState.Board[w, h] == 'O')
+                    {
+                        oScore += 0.02f;
+                    }
+                }
+            }
+
+        }
+        else    // if no moves remain the game is over, and the evaluation should be how many pieces each player has
+        {
+            // check for number of tiles possesed
+            for (int h = 0; h < board.GetLength(1); h++)
+            {
+                for (int w = 0; w < board.GetLength(0); w++)
+                {
+                    if (gameState.Board[w, h] == 'x')
+                    {
+                        xScore += 1;
+                    }
+
+                    if (gameState.Board[w, h] == 'X')
+                    {
+                        xScore += 2;
+                    }
+
+                    if (gameState.Board[w, h] == 'o')
+                    {
+                        oScore += 1;
+                    }
+
+                    if (gameState.Board[w, h] == 'O')
+                    {
+                        oScore += 2;
+                    }
+                }
+            }
+        }
+
+        // return difference in scores rounded to nearest int
+        return Mathf.RoundToInt(xScore - oScore);
+    }
+
+    int EvaluateEndGame(GameState gameState)
+    {
+        int score = 0;
+
+        int p1Score = 0;
+        int p2Score = 0;
+
+        
+        Debug.Log("Score Start - P1 score: " + p1Score + " P2 score: " + p2Score);
+
+        for (int h = 0; h < gameState.Board.GetLength(1); h++)
+        {
+            for (int w = 0; w < gameState.Board.GetLength(0); w++)
+            {
+                if (gameState.Board[w, h] == 'x')
+                {
+                    p1Score++;
+                }
+
+                if (gameState.Board[w, h] == 'X')
+                {
+                    p1Score += 2;
+                }
+
+                if (gameState.Board[w, h] == 'o')
+                {
+                    p2Score++;
+                }
+
+                if (gameState.Board[w, h] == 'O')
+                {
+                    p2Score += 2;
+                }
+            }
+        }
+        Debug.Log("Score end - P1 score: " + p1Score + " P2 score: " + p2Score);
+
+        
+        //if (p1Score > p2Score)  //check if X wins
+        //{
+        //    score = WIN_SCORE;
+        //}
+        //else if (p1Score < p2Score)                       //check if O wins
+        //{
+        //    outcome = 'o';
+        //}
+        //else if (p1Score == p2Score)  //check if its a draw
+        //{
+        //    score +=;
+        //}
+        return 1;
+    }
+
+
+    int EvaluateMobility(GameState gameState)
     {
         return 1;
     }
+
+
+
+    //public bool IsGameOver()
+    //{
+    //    List<Vector2Int> xLegalMoves = GetPossibleMoves('x');
+    //    if (xLegalMoves.Count == 0)  // Check if playerX has no moves left (if they aren't traped by soild tiles)
+    //    {
+    //        return true;
+    //    }
+
+    //    List<Vector2Int> oLegalMoves = GetPossibleMoves('o');
+    //    if (oLegalMoves.Count == 0)  // Check if playerO has no moves left (if they aren't traped by soild tiles)
+    //    {
+    //        return true;
+    //    }
+
+    //    for (int h = 0; h < board.GetLength(1); h++)
+    //    {
+    //        for (int w = 0; w < board.GetLength(0); w++)
+    //        {
+    //            if (board[w, h] == '.')  // check if there are any empty tiles left 
+    //            {
+    //                return false;
+    //            }
+    //        }
+    //    }
+    //    return true;
+    //}
+
+    //public char GetGameOutcome()
+    //{
+    //    char outcome;
+    //    p1Score = 0;
+    //    p2Score = 0;
+
+    //    Debug.Log("Score Start - P1 score: " + p1Score + " P2 score: " + p2Score);
+
+    //    for (int h = 0; h < board.GetLength(1); h++)
+    //    {
+    //        for (int w = 0; w < board.GetLength(0); w++)
+    //        {
+    //            if (board[w, h] == 'x')
+    //            {
+    //                p1Score++;
+    //            }
+
+    //            if (board[w, h] == 'X')
+    //            {
+    //                p1Score += 2;
+    //            }
+
+    //            if (board[w, h] == 'o')
+    //            {
+    //                p2Score++;
+    //            }
+
+    //            if (board[w, h] == 'O')
+    //            {
+    //                p2Score += 2;
+    //            }
+    //        }
+    //    }
+    //    Debug.Log("Score end - P1 score: " + p1Score + " P2 score: " + p2Score);
+
+    //    if (p1Score == p2Score)  //check if its a draw
+    //    {
+    //        outcome = 'd';
+    //    }
+    //    else if (p1Score > p2Score)  //check if X wins
+    //    {
+    //        outcome = 'x';
+    //    }
+    //    else                        //check if O wins
+    //    {
+    //        outcome = 'o';
+    //    }
+    //    return outcome;
+    //}
 }
